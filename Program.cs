fd9ff1ea2a17e60428c63ee8ef677ea940b60bc2
wojas2000 <96478846+wojas2000@users.noreply.github.com>
2022-03-10 20:36:28 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Math;
+
+namespace Simple_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {            
+            //Program calculate divide, addition, subtraction, multiplication two numbers           
+            double a, b;
+            Console.WriteLine("Prosty kalkulator który dzieli, dodaje, odejmuje i mnoży dwie liczby.");
+            a = double.Parse(Console.ReadLine());
+            b = double.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Wyniki:");
+            Console.WriteLine();
+            Console.WriteLine("a/b = {0:#.##}", a / b);
+            Console.WriteLine("a+b = {0:#.##}", a + b);
+            Console.WriteLine("a-b = {0:#.##}", a - b);
+            Console.WriteLine("a*b = {0:#.##}", a * b);
+        }
+    }
+}
